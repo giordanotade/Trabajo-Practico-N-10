2ede84a64761f6af96a78a278467d5dd7000466f
giordanotade <tadegiordano@gmail.com>
2023-09-06 20:40:02 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*function showAlertOnMobile() {
+  // Verificar si la pantalla tiene formato de tablet o celular
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    alert("Para mayor comodidad y mejor lectura, presione un elemento para ver mejor");
+  }
+}
+
+//esto se ve feo//
+//qué decis//
+
+// Ejecutar la función cuando la página se carga y cuando la ventana cambia de tamaño
+window.addEventListener("load", showAlertOnMobile);
+window.addEventListener("resize", showAlertOnMobile);
